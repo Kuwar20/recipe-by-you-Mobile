1c2b7a50ed79108661a45b08cdcd785935a789e4
Kuwar20 <kuwarx1@gmail.com>
2024-01-18 00:04:21 +0530

    Add animation and navigation to WelcomeScreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,31 +1,65 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 export default function WelcomeScreen() {
+    const ring1padding = useSharedValue(0);
+    const ring2padding = useSharedValue(0);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        ring1padding.value = 0;
+        ring2padding.value = 0;
+
+        setTimeout(
+            () => (ring1padding.value = withSpring(ring1padding.value + hp(5))),
+            100
+        );
+        setTimeout(
+            () => (ring2padding.value = withSpring(ring2padding.value + hp(6))),
+            300
+        );
+
+        setTimeout(() => navigation.navigate("Home"), 1000);
+    }, []);
+
     return (
         <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
             <StatusBar style="light" />
             {/* logo image with rings */}
-            <View className="bg-white/20 rounded-full" style={{ padding: hp(5.5) }}>
-                <View className="bg-white/20 rounded-full p-7" style={{ padding: hp(5) }}>
+            <Animated.View
+                className="bg-white/20 rounded-full"
+                style={{ padding: ring2padding }}
+            >
+                <Animated.View
+                    className="bg-white/20 rounded-full"
+                    style={{ padding: ring1padding }}
+                >
                     <Image
                         source={require("../../assets/images/welcome.png")}
                         style={{ width: hp(20), height: hp(20) }}
                     />
-                </View>
-            </View>
+                </Animated.View>
+            </Animated.View>
             {/* title and punchline */}
             <View className="flex items-center space-y-2">
-                <Text style={{ fontSize: hp(7) }} className="font-bold text-white tracking-widest">
+                <Text
+                    style={{ fontSize: hp(7) }}
+                    className="font-bold text-white tracking-widest"
+                >
                     Recipe
                 </Text>
-                <Text style={{ fontSize: hp(2) }} className="font-medium text-white tracking-widest">
+                <Text
+                    style={{ fontSize: hp(2) }}
+                    className="font-medium text-white tracking-widest"
+                >
                     Food is always right
                 </Text>
             </View>

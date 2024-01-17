db1bb6546f79b36ab8251340c3ea7436e95acfe4
Kuwar20 <kuwarx1@gmail.com>
2024-01-17 20:59:17 +0530

    Update Babel configuration and dependencies

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <Text>Hello React Native </Text>
       <StatusBar style="auto" />
     </View>
   );
